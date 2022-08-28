8d340d86536ac35d70204be238bb23f86d3d6d03
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-08-27 22:58:53 -0300

    feat: add next rewrites

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
 	images: {
 		domains: ['github.com'],
 	},
+	async rewrites() {
+		return [
+			{
+				source: '/og-image.png',
+				destination: '/api/og-image',
+				has: [{ type: 'query', key: 'title' }],
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
