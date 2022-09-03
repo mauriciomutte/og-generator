d0e48e9e71a8ed9a1929e54549b5958bbd12f62b
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-09-03 17:00:49 -0300

    fix: remove chromium functions return types

--- src/lib/chromium.ts
+++ src/lib/chromium.ts
@@ -4,24 +4,19 @@ import { getOptions } from './options';
 
 let _page: Page | null;
 
-async function getPage(isDev: boolean): Promise<Page> {
+async function getPage(isDev: boolean) {
 	if (_page) {
 		return _page;
 	}
 
 	const options = await getOptions(isDev);
-
 	const browser = await puppeteer.launch(options);
-
 	_page = await browser.newPage();
 
 	return _page;
 }
 
-export async function getScreenshot(
-	url: string,
-	isDev: boolean
-): Promise<Buffer> {
+export async function getScreenshot(url: string, isDev: boolean) {
 	const page = await getPage(isDev);
 
 	await page.setViewport({ width: 1200, height: 630 });
